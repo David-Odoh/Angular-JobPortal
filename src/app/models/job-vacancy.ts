0bba5773397bf6db4463f9f97bc7d79a8a7c5c44
David Odoh <david.odoh1@gmail.com>
2018-09-25 03:38:14 +0100

    Added Models

---
+++ src/app/models/job-vacancy.ts
@@ -0,0 +1,18 @@
+export class vacancyModel {
+    id: string;
+    title: string;
+    datePosted: Date;
+    category: string;
+    subCategory: string;
+    vacacyType: string;
+}
+
+export class category {
+    id: string;
+    name: string;
+}
+
+export class subCategory {
+    id: string;
+    name: string;
+}
---
+++ src/app/models/user.ts
@@ -0,0 +1,13 @@
+export class User {
+    id: string;
+    name: string;
+    email: string;
+    password: string
+}
+
+export class vacancyType {
+    types = [
+        { id: 1, name: 'featured' },
+        { id: 2, name: '' }
+    ]
+}
